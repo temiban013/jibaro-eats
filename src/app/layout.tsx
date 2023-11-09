dff49ed5698761d7246da71b0b73888db6deeaaf
Mario Rafael Ayala <137118271+temiban013@users.noreply.github.com>
2023-11-09 18:09:48 -0400

    updated social media image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,13 +23,13 @@ export const metadata: Metadata = {
       description: 'Eating Jibaro-style. Digital services for eateries.',
       title: 'Jibaro Eats',
       locale: 'en-US',
-      images: '/og-image.png',
+      images: 'https://res.cloudinary.com/drc0myo7z/image/upload/c_scale,w_1080/v1694092635/Jibaro-Eats/SAM_7866_b7ummj.jpg',
       videos: 'https://www.youtube.com/watch?v=ti2ob5111g8'
   },
   twitter: {
       card: 'summary_large_image',
       title: 'Jibaro Eats',
-      images: '/og-image.png',
+      images: 'https://res.cloudinary.com/drc0myo7z/image/upload/c_scale,w_1080/v1694092635/Jibaro-Eats/SAM_7866_b7ummj.jpg',
       description: 'Eating Jibaro-style. Digital services for eateries.',
   }
 }
