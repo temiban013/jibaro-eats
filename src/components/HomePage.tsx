bc4d98cf13c8b75053418de299dd97c8db24b412
Mario Rafael Ayala <137118271+temiban013@users.noreply.github.com>
2023-11-09 17:59:14 -0400

    Added Title to splash screen

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -16,6 +16,13 @@ export default function HomePage({images}: { images: ImageProps[] }) {
       <>
         <main className="mx-auto max-w-[1960px] p-4">
           <div className="columns-1 gap-4 sm:columns-2 xl:columns-3 2xl:columns-4">
+            <div className="after:content group relative mb-5 block w-full after:pointer-events-none">
+              <div className="text-center bg-black text-white/80 sm:text-left sm:pl-12 sm:pt-12 sm:pb-6">
+                <h1 className="text-4xl font-bold sm:text-5xl md:text-6xl">Jíbaro Eats</h1>
+                <p className="text-xl sm:text-2xl"><a href="tel:+17879328884" className="sm:inline-block">(787) 932-0884</a> </p>
+                <p className="text-xl sm:text-2xl"><a href="mailto:jibaromg@gmail.com" className="sm:inline-block">jibaromg@gmail.com</a></p>
+              </div>
+            </div>
             {images.map(({ id, public_id, format, blurDataUrl }) => (
               <div key={id} className="after:content group relative mb-5 block w-full after:pointer-events-none">
                 <Image
